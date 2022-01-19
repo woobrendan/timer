47e88ba3060c6e25dc3a405a1555668fec3baee4
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 01:57:24 +0000

    lint, added semi colons

--- timer2.js
+++ timer2.js
@@ -15,15 +15,15 @@ const numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 stdin.on('data', (key) => {
   for (let num of numberArray) {
     if (key === num) {
-      console.log(`Setting timer for ${num} seconds...`)
+      console.log(`Setting timer for ${num} seconds...`);
       setTimeout(() => process.stdout.write('\x07'), (Number(num)) * 1000);
     }
   }
   if (key === 'b') {
-    process.stdout.write('\x07')
+    process.stdout.write('\x07');
   }
   if (key === '\u0003') {
     console.log('Thanks for using me, ciao!');
     process.exit();
   }
-})
+});
